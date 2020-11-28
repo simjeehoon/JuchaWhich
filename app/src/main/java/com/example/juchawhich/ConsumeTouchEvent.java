834be10047a8b2a14c4d9e212d72c10b9dbf756e
simjeehoon <simjeehoon@naver.com>
2020-11-28 21:43:10 +0900

    made top, bottom msg animation

---
+++ app/src/main/java/com/example/juchawhich/ConsumeTouchEvent.java
@@ -0,0 +1,11 @@
+package com.example.juchawhich;
+
+import android.view.MotionEvent;
+import android.view.View;
+
+public class ConsumeTouchEvent implements View.OnTouchListener {
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        return true;
+    }
+}
