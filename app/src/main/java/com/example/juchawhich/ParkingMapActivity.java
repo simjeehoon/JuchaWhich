b8de397e32cb127f65e5b854321bcccd481f362e
simjeehoon <simjeehoon@naver.com>
2020-11-30 03:47:29 +0900

    separate toolbar class on parking map activity

--- app/src/main/java/com/example/juchawhich/ParkingMapActivity.java
+++ app/src/main/java/com/example/juchawhich/ParkingMapActivity.java
@@ -22,27 +22,19 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class ParkingMapActivity extends AppCompatActivity implements OnMapReadyCallback {
-    Toolbar toolbar;
-    ActionBar actionBar;
+
     ParkingMapSlideMenu parkingMapSlideMenu;
     ParkingMapMessageBox parkingMapMessageBox;
+    ParkingMapToolbar parkingMapToolbar;
 
-    private void setActionBar(){
-        toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        actionBar = getSupportActionBar();
-        actionBar.setDisplayShowCustomEnabled(true);
-        actionBar.setDisplayShowTitleEnabled(false);//기본 제목을 없애줍니다.
-        actionBar.setDisplayHomeAsUpEnabled(true);//좌측 버튼 설정
-        actionBar.setHomeAsUpIndicator(R.drawable.menu_icon);//좌측 버튼 아이콘 설정
+    public ParkingMapSlideMenu getParkingMapSlideMenu(){
+        return parkingMapSlideMenu;
     }
 
     private void setMapScreen(){
         FragmentManager fragmentManager = getFragmentManager();
         MapFragment mapFragment = (MapFragment)fragmentManager.findFragmentById(R.id.google_map);
         mapFragment.getMapAsync(this);
-
-        Log.d("googlemapLog", "단계1");
     }
 
     @Override
@@ -56,24 +48,21 @@ public class ParkingMapActivity extends AppCompatActivity implements OnMapReadyC
 
         map.moveCamera(CameraUpdateFactory.newLatLng(Seoul));
         map.animateCamera(CameraUpdateFactory.zoomTo(10));
-        Log.d("googlemapLog", "단계2");
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parking_map);
-        setActionBar();
+        parkingMapToolbar = new ParkingMapToolbar(this);
         parkingMapSlideMenu = new ParkingMapSlideMenu(this);
         parkingMapMessageBox = new ParkingMapMessageBox(this);
-
         setMapScreen();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.parking_screen_menu, menu);
+        getMenuInflater().inflate(R.menu.parking_screen_menu, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
@@ -86,7 +75,7 @@ public class ParkingMapActivity extends AppCompatActivity implements OnMapReadyC
                 parkingMapSlideMenu.closeRightMenu();
             } else if(System.currentTimeMillis()-initTime > 3000){
                 Toast.makeText(getApplicationContext(), "종료하려면 한번 더 누르세요", Toast.LENGTH_SHORT).show();
-                initTime=System.currentTimeMillis();
+                initTime = System.currentTimeMillis();
             } else {
                 finish();
             }
@@ -96,31 +85,7 @@ public class ParkingMapActivity extends AppCompatActivity implements OnMapReadyC
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
-           case R.id.auto_parking_menu:
-                Toast.makeText(getApplicationContext(), "자동주차", Toast.LENGTH_SHORT).show();
-                parkingMapMessageBox.showMessageBox();
-                break;
-            case R.id.share_car_menu:
-                Toast.makeText(getApplicationContext(), "차량공유", Toast.LENGTH_SHORT).show();
-                parkingMapMessageBox.hideMessageBox();
-                break;
-            case R.id.parking_record_menu:
-                Toast.makeText(getApplicationContext(), "주차기록", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.car_info_menu:
-                Toast.makeText(getApplicationContext(), "차량정보", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.add_car:
-                Toast.makeText(getApplicationContext(), "차량 추가", Toast.LENGTH_SHORT).show();
-                break;
-            case android.R.id.home:
-                if(!parkingMapSlideMenu.isMenuOpen()) {
-                    parkingMapSlideMenu.openRightMenu();
-                }
-                break;
-        }
+        parkingMapToolbar.onOptionsItemSelected(item);
         return super.onOptionsItemSelected(item);
     }
-
 }
---
+++ app/src/main/java/com/example/juchawhich/ParkingMapToolbar.java
@@ -0,0 +1,53 @@
+package com.example.juchawhich;
+
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.widget.Toolbar;
+
+public class ParkingMapToolbar {
+    ParkingMapActivity mainActivity;
+
+    Toolbar toolbar;
+    ActionBar actionBar;
+
+    public ParkingMapToolbar(ParkingMapActivity appCompatActivity){
+        mainActivity = appCompatActivity;
+
+        toolbar = mainActivity.findViewById(R.id.toolbar);
+        mainActivity.setSupportActionBar(toolbar);
+        actionBar = mainActivity.getSupportActionBar();
+
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayShowTitleEnabled(false); //기본 제목을 없애줍니다.
+        actionBar.setDisplayHomeAsUpEnabled(true); //좌측 버튼 설정
+        actionBar.setHomeAsUpIndicator(R.drawable.menu_icon); //좌측 버튼 아이콘 설정
+    }
+
+    public void onOptionsItemSelected(@NonNull MenuItem item){
+        switch (item.getItemId()){
+            case R.id.auto_parking_menu:
+                Toast.makeText(mainActivity.getApplicationContext(), "자동주차", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.share_car_menu:
+                Toast.makeText(mainActivity.getApplicationContext(), "차량공유", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.parking_record_menu:
+                Toast.makeText(mainActivity.getApplicationContext(), "주차기록", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.car_info_menu:
+                Toast.makeText(mainActivity.getApplicationContext(), "차량정보", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.add_car:
+                Toast.makeText(mainActivity.getApplicationContext(), "차량 추가", Toast.LENGTH_SHORT).show();
+                break;
+            case android.R.id.home:
+                if(!mainActivity.getParkingMapSlideMenu().isMenuOpen()) {
+                    mainActivity.getParkingMapSlideMenu().openRightMenu();
+                }
+                break;
+        }
+    }
+}
